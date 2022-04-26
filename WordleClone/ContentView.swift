106642c873119aebcbbcaf854ac7ef1a35c35775
StealthElf98 <sergej.dragojlovic@gmail.com>
2022-04-26 21:16:38 +0200

    Initial Commit

---
+++ WordleClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordleClone
+//
+//  Created by Sergej on 26.4.22..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WordleClone/WordleCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleCloneApp.swift
+//  WordleClone
+//
+//  Created by Sergej on 26.4.22..
+//
+
+import SwiftUI
+
+@main
+struct WordleCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
